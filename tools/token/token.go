2253f010aba4b21c012abb967ef3431dfe50cea7
Furl0w <rainbowphoque@gmail.com>
2019-01-21 16:31:08 +0100

    specifying secret for token library

--- tools/token/token.go
+++ tools/token/token.go
@@ -16,10 +16,10 @@ type clientClaims struct {
 //CreateToken creates a token from an email and a time duration. The token will expire in the expiration provided minutes from now.
 //
 //TODO -> change the email for a user structure for passing parameters to the client
-func CreateToken(email string, expiration time.Duration) (string, error) {
+func CreateToken(email string, expiration time.Duration, secret []byte) (string, error) {
 	newClaims := clientClaims{email, jwt.StandardClaims{ExpiresAt: time.Now().Add(time.Minute * expiration).Unix()}}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &newClaims)
-	tokenString, err := token.SignedString([]byte("secret"))
+	tokenString, err := token.SignedString(secret)
 	if err != nil {
 		log.Println(err)
 		return "", err
@@ -29,12 +29,12 @@ func CreateToken(email string, expiration time.Duration) (string, error) {
 
 //ValidateToken validate a token by : validating the signing method is HMAC,
 //validating the key, checking if the token is not expired and checking that the client match the email in the token
-func ValidateToken(tokenString string, client string) (string, error) {
+func ValidateToken(tokenString string, client string, secret []byte) (string, error) {
 	token, _ := jwt.ParseWithClaims(tokenString, &clientClaims{}, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("There was an error")
 		}
-		return []byte("secret"), nil
+		return secret, nil
 	})
 	if claims, ok := token.Claims.(*clientClaims); ok && token.Valid {
 		if claims.Email == client && time.Unix(claims.StandardClaims.ExpiresAt, 0).After(time.Now()) {
